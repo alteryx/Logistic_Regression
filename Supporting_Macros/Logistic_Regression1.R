b5c4d682e7e1c5906d3f1b751daed2ce8f4976d9
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-09-26 11:16:13 -0700

    reorganize code to support auto-testing.

--- Supporting_Macros/Logistic_Regression1.R
+++ Supporting_Macros/Logistic_Regression1.R
@@ -25,50 +25,52 @@ options(alteryx.wd = '%Engine.WorkflowDirectory%')
 options(alteryx.debug = config$debug)
 ##----
 
-#' Modify the link so that it can be passed on to R.
-
-if (config$the.link == "complementary log-log"){
-  config$the.link <- "cloglog"
-}
-
 #' #### Read Inputs
 #'
 #' This is a named list of all inputs that stream into the R tool.
 #' We also specify defaults for use when R code is run outside Alteryx.
+iris$Species <- as.factor(ifelse(iris$Species != "setosa", "other", "setosa"))
 defaults <- list(
-  data = transform(iris,
-    Species = as.factor(ifelse(Species != "setosa", "other", "setosa"))
-  )[,5:1]
+  data = iris[,c(config$`Y Var`, config$`X Vars`)]
 )
 inputs <- list(
-  the.data = read.Alteryx2("#1", default = defaults$data)
+  the.data = read.Alteryx2("#1", default = defaults$data),
+  XDFInfo = getXdfProperties("#1", default = list(is_XDF = FALSE, xdf_path = NULL))
 )
 
 #' ### Run and Create Outputs
-#' Load Libraries
-#+ warning=FALSE
-library(car)
-XDFInfo <- if (inAlteryx()) getXdfProperties("#1") else list(is_XDF = FALSE)
-if (XDFInfo$is_XDF){
-  d <- processLogisticXDF(inputs, config)
-  glm.out <- createReportLogisticXDF(d$the.model, config, d$null.model)
-  plot.out <- function(){createPlotOutputsLogisticXDF()}
-} else {
-  d <- processLogisticOSR(inputs, config)
-  glm.out <- createReportLogisticOSR(d$the.model, config, d$model_type)
-  plot.out <- function(){createPlotOutputsLogisticOSR(d$the.model, FALSE, config)}
+runLogisticRegression <- function(inputs, config){
+  library(car)
+  
+  #' Modify the link so that it can be passed on to R.
+  if (config$the.link == "complementary log-log"){
+    config$the.link <- "cloglog"
+  }
+  
+  if (inputs$XDFInfo$is_XDF){
+    d <- processLogisticXDF(inputs, config)
+    glm.out <- createReportLogisticXDF(d$the.model, config, d$null.model)
+    plot.out <- function(){createPlotOutputsLogisticXDF()}
+  } else {
+    d <- processLogisticOSR(inputs, config)
+    glm.out <- createReportLogisticOSR(d$the.model, config, d$model_type)
+    plot.out <- function(){
+      createPlotOutputsLogisticOSR(d$the.model, FALSE, config)
+    }
+  }
+  
+  # Report Output
+  write.Alteryx2(glm.out, nOutput = 1)
+  
+  # Plot Output
+  whr <- graphWHR2(inches = TRUE, in.w = 6, in.h = 6, config$graph.resolution)
+  AlteryxGraph2(plot.out(), 2, width = whr[1], height = whr[2],
+    res = whr[3], pointsize = 9)
+  
+  # Model Output
+  the.obj <- prepModelForOutput(config$model.name, d$the.model)
+  write.Alteryx2(the.obj, nOutput = 3)  
 }
 
-# Report Output
-write.Alteryx2(glm.out, nOutput = 1)
+runLogisticRegression(inputs, config)
 
-# Plot Output
-# whr <- graphWHR(inches = "True", in.w = 6, in.h = 6, config$resolution)
-whr <- AlteryxPredictive:::graphWHR2(inches = TRUE, in.w = 6, in.h = 6,
-  config$graph.resolution)
-AlteryxGraph2(plot.out(), 2, width = whr[1], height = whr[2],
-  res = whr[3], pointsize = 9)
-
-# Model Output
-the.obj <- prepModelForOutput(config$model.name, d$the.model)
-write.Alteryx2(the.obj, nOutput = 3)
