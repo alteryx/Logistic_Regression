1cd539e27a692fe3c1268af9990893097541b061
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-09-27 15:34:11 -0700

    update config names and use AlteryxPredictive.

--- Supporting_Macros/Logistic_Regression1.R
+++ Supporting_Macros/Logistic_Regression1.R
@@ -14,9 +14,9 @@
 suppressWarnings(library(AlteryxPredictive))
 config <- list(
   `graph.resolution` = dropdownInput('%Question.graph.resolution%' , '1x'),
-  `the.link` = dropdownInput('%Question.Link%' , 'logit'),
-  `model.name` = textInput('%Question.Model Name%'),
-  `used.weights` = checkboxInput('%Question.Use Weights%' , FALSE),
+  `Link` = dropdownInput('%Question.Link%' , 'logit'),
+  `Model Name` = textInput('%Question.Model Name%'),
+  `Use Weights` = checkboxInput('%Question.Use Weights%' , FALSE),
   `Weight Vec` = dropdownInput('%Question.Weight Vec%'),
   `X Vars` = listInput('%Question.X Vars%', c('Sepal.Length', 'Petal.Length')),
   `Y Var` = dropdownInput('%Question.Y Var%', 'Species')
@@ -38,39 +38,6 @@ inputs <- list(
   XDFInfo = getXdfProperties("#1", default = list(is_XDF = FALSE, xdf_path = NULL))
 )
 
-#' ### Run and Create Outputs
-runLogisticRegression <- function(inputs, config){
-  library(car)
-  
-  #' Modify the link so that it can be passed on to R.
-  if (config$the.link == "complementary log-log"){
-    config$the.link <- "cloglog"
-  }
-  
-  if (inputs$XDFInfo$is_XDF){
-    d <- processLogisticXDF(inputs, config)
-    glm.out <- createReportLogisticXDF(d$the.model, config, d$null.model)
-    plot.out <- function(){createPlotOutputsLogisticXDF()}
-  } else {
-    d <- processLogisticOSR(inputs, config)
-    glm.out <- createReportLogisticOSR(d$the.model, config, d$model_type)
-    plot.out <- function(){
-      createPlotOutputsLogisticOSR(d$the.model, FALSE, config)
-    }
-  }
-  
-  # Report Output
-  write.Alteryx2(glm.out, nOutput = 1)
-  
-  # Plot Output
-  whr <- graphWHR2(inches = TRUE, in.w = 6, in.h = 6, config$graph.resolution)
-  AlteryxGraph2(plot.out(), 2, width = whr[1], height = whr[2],
-    res = whr[3], pointsize = 9)
-  
-  # Model Output
-  the.obj <- prepModelForOutput(config$model.name, d$the.model)
-  write.Alteryx2(the.obj, nOutput = 3)  
-}
 
-runLogisticRegression(inputs, config)
+AlteryxPredictive:::runLogisticRegression(inputs, config)
 
